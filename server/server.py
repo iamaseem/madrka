a3dad1696d260f1442d9139a089fe234126b850a
ASEEMMUHAMMAD A. R <asaseemas@gmail.com>
2021-01-31 13:41:12 +0530

    completed api code 1V

--- server/server.py
+++ server/server.py
@@ -1,30 +1,48 @@
-from flask import Flask, request
-from flask_cors import CORS
+import json
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import date
+
+from flask import Flask, request, jsonify
+from flask_api import status
+
+import sqlite3
+from sqlite3 import Error
+
+
+con = sqlite3.connect('mydb.db')
+cur = con.cursor()
 
 app = Flask(__name__)
-CORS(app)
+app.config['SECRET_KEY'] = 'secret-key-goes-here'
 
-utxt = open("user.txt", "r")
-ptxt = open("pass.txt", "r")
+
+@app.route('/')
+def index():
+    return 'hello world'
+
 
 @app.route('/api/login', methods=['POST'])
 def login():
-    user = request.form['user']
-    password = request.form['password']
-    i = 0
-    data = "failed"
-    lines = utxt.readlines()
-    while i<len(lines):
-        line = lines[i].rstrip('\n')
-        if user == line:
-            all_pass = ptxt.readlines()
-            pas =  all_pass[i].rstrip('\n')
-            if password == pas:
-                data = "success"
-            else:
-                data = "failed"
-        i = i+1
+	record = json.loads(request.data)
+	username = record['username']
+	password = record['password']
+	
+	cur.execute('SELECT password FROM user WHERE username=?', [username])
 
-    return data
+	result = cur.fetchone()
 
-app.run(debug=True)
+	if not result or not check_password_hash(result[0], password):
+		result = {"status": status.HTTP_406_NOT_ACCEPTABLE, "desc": "Login Failed"}
+		return result
+
+	hash = generate_password_hash(username+password)
+	result1 = {
+		"status": status.HTTP_200_OK,
+		"hash": hash,
+		"date": str(date.today().day) + "-" + str(date.today().month) + "-" + str(date.today().year),
+		"APIv": "0.1.0"
+		}
+	return jsonify(result1)
+
+if __name__ == '__main__':
+    app.run(debug=True)
